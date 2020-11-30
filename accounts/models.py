25c87b8a6049f6e4311ac373328a1f5b2fddd62c
c03rcion <areeves9@icloud.com>
2020-11-29 21:52:15 -0800

    SiteUserManager for custom SiteUser model

--- accounts/models.py
+++ accounts/models.py
@@ -2,17 +2,49 @@ from django.db import models
 from django.contrib.auth.models import (
     BaseUserManager, AbstractBaseUser
 )
-from django.contrib.auth.models import UserManager
+
+
+class SiteUserManager(BaseUserManager):
+    def create_user(self, email, password=None):
+        """
+        Creates and saves a User with the given email and password.
+        """
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            email=self.normalize_email(email),
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None):
+        """
+        Creates and saves a superuser with the given email and password.
+        """
+        user = self.create_user(
+            email,
+            password=password,
+        )
+        user.is_admin = True
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
 
 
 class SiteUser(AbstractBaseUser):
-    objects = UserManager()
-    
+    objects = SiteUserManager()
+
     email = models.EmailField(
         verbose_name='email address',
         max_length=255,
         unique=True
     )
+    first_name = models.CharField(max_length=150, blank=True)
+    last_name = models.CharField(max_length=150, blank=True)
     is_active = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
